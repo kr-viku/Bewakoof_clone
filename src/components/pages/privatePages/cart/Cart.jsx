573d3072fa011ebcd1a5049695890181eeff3244
Priti201 <Priti201>
2023-11-25 09:10:14 +0530

    created empty cart UI

--- src/components/pages/privatePages/cart/Cart.jsx
+++ src/components/pages/privatePages/cart/Cart.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import './Cart.css'
+import React from "react";
+import "./Cart.css";
+import { Button, Image } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 const Cart = () => {
-  return (
-    <div className='cart-wrapper'>Cart</div>
-  )
-}
+  const navigate = useNavigate();
+  const cartItems = 0;
+  if (cartItems !== 0) {
+    return <h2>cartItems present</h2>;
+  } else {
+    return (
+      <div className="empty-cart-wrapper">
+        <Image
+          src="https://images.bewakoof.com/images/doodles/empty-cart-page-doodle.png"
+          alt="empty-cart"
+          fluid
+          width="15%"
+        />
+        <div className="empty-list-title">Nothing in the bag</div>
+        <div className="empty-list-subtitle">
+          Add your product here and make them yours soon!
+        </div>
+        <Button type="button" onClick={() => navigate("/")}>
+          Shop Now
+        </Button>
+      </div>
+    );
+  }
+};
 
-export default Cart
+export default Cart;
--- src/components/pages/privatePages/wishlist/Wishlist.jsx
+++ src/components/pages/privatePages/wishlist/Wishlist.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './Wishlist.css'
+import { Button, Image } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Wishlist = () => {
-  return (
-    <div>Wishlist</div>
+    const navigate = useNavigate();
+  const cartItems = 0;
+  if (cartItems !== 0) {
+    return <h2>cartItems present</h2>;
+  } else {
+    return (
+      <div className="empty-wishlist-wrapper">
+        <Image
+          src="https://images.bewakoof.com/web/wishlistEmpty.svg"
+          alt="empty-cart"
+          fluid
+          width="10%"
+        />
+        <div className="empty-wishlist-title">Hey! Your wishlist is empty.</div>
+        <div className="empty-wishlist-subtitle">
+        Save your favourites here and make them yours soon!
+        </div>
+        <Button type="button" onClick={() => navigate("/")}>
+          Shop Now
+        </Button>
+      </div>
   )
+  }
 }
 
 export default Wishlist
