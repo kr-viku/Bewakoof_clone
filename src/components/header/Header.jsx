5b68218f97333c9b0d6fc1e616d6c78b29c27ef9
Priti201 <Priti201>
2023-12-01 08:37:03 +0530

    updated tobBar css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import NavBar from "./navBar/NavBar";
 import TopBar from "./topBar/TopBar";
 
-const Header = ({isSearching, setIsSearching}) => {
+const Header = ({ isSearching, setIsSearching }) => {
   return (
-    <div id="header-container">
-      <TopBar />
-      <NavBar isSearching={isSearching} setIsSearching= {setIsSearching} />
+    <div className="header-wrapper">
+        <TopBar />
+        {/* <NavBar isSearching={isSearching} setIsSearching={setIsSearching} /> */}
+    
     </div>
   );
 };
--- src/components/header/topBar/TopBar.jsx
+++ src/components/header/topBar/TopBar.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import PhoneIphoneOutlinedIcon from '@mui/icons-material/PhoneIphoneOutlined';
-import './TopBar.css';
+import React from "react";
+import PhoneIphoneOutlinedIcon from "@mui/icons-material/PhoneIphoneOutlined";
+import "./TopBar.css";
 const TopBar = () => {
   return (
-    <div id='topBar-container'>
+    <div className="topBar-wrapper">
+      <div className="topBar-container">
         <div>
-       <ul className='left-items'>
-         <li>Offers</li>
-         <li>Fanbook</li>
-         <li> Download App</li>
-         <li>TriBe Membership</li>
-       </ul>
-       </div>
-       <div>
-       <ul className='right-items'>
-         <li>Contact Us</li>
-         <li>Track Order</li>
-       </ul>
-       </div>
+          <ul className="topBar-items">
+            <li>Offers</li>
+            <li>Fanbook</li>
+            <li> Download App</li>
+            <li>TriBe Membership</li>
+          </ul>
+        </div>
+        <div>
+          <ul className="topBar-items">
+            <li>Contact Us</li>
+            <li>Track Order</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
